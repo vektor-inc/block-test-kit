d9680ff162a579856902a3b919e9fc20674d1e89
Naoki Ohashi <n.globe.us@gmail.com>
2020-12-11 09:21:28 +0900

    Update jest.config.js

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+	preset: '@wordpress/jest-preset-default',
 	transform : {
 		"^.+\\.js$": "babel-jest",
 		".+\\.(svg|css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub"
