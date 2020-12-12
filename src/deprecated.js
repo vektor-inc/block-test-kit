7094299cc782f0a3e789f2c7c636f27b6756dfd9
naoki <n.globe.us@gmail.com>
2020-12-12 11:34:32 +0900

    [Fix] deprecated attributes

--- src/deprecated.js
+++ src/deprecated.js
@@ -1,16 +1,14 @@
 import { RichText, useBlockProps } from '@wordpress/block-editor';
 
 const blockAttributes = {
-	attributes: {
-		style: {
-			type: 'string',
-			default: 'info',
-		},
-		content: {
-			type: 'string',
-			source: 'html',
-			selector: 'p',
-		},
+	style: {
+		type: 'string',
+		default: 'info',
+	},
+	content: {
+		type: 'string',
+		source: 'html',
+		selector: 'p',
 	},
 };
 
@@ -20,6 +18,7 @@ const deprecated = [
 		save({ attributes }) {
 
 			const { style, content } = attributes;
+
 			return (
 				<div { ...useBlockProps.save( { className: `alert alert-${style}`} ) }>
 					<RichText.Content tagName={'p'} value={content} />
